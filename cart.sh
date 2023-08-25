d4f3577f6743fcfa3af77536712a67309022ee86
Raghunath <rmorampu@gmail.com>
2023-08-25 18:00:45 +0800

    pushing cart.sh file to git

---
+++ cart.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "Setting up NPM source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "Installing NodeJS"
+
+useradd roboshop &>>$LOGFILE
+
+
+mkdir /app &>>$LOGFILE
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>$LOGFILE
+
+VALIDATE $? "downloading cart artifact"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving into app directory"
+
+unzip /tmp/cart.zip &>>$LOGFILE
+
+VALIDATE $? "unzipping catalogue"
+
+npm install 
+
+VALIDATE $? "Installing dependencies"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon-reload"
+
+systemctl enable cart  &>>$LOGFILE
+
+VALIDATE $? "Enabling Cart"
+
+systemctl start cart &>>$LOGFILE
+
+VALIDATE $? "Starting Cart"
